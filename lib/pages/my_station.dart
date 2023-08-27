e7e08bde9f54bf6189ea0473f07b72cc48ab0e1a
Gagan Bindal <gaganbindal2002@gmail.com>
2023-08-28 01:38:00 +0530

    bottom sheet

--- lib/pages/my_station.dart
+++ lib/pages/my_station.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:krishakthi/components/card.dart';
 import 'package:krishakthi/components/equipment_details.dart';
 import 'package:krishakthi/components/map_icons.dart';
+import 'package:wtf_sliding_sheet/wtf_sliding_sheet.dart';
 
 const LatLng currentLocation = LatLng(31.224020, 75.770798);
 class MyStation extends StatefulWidget {
@@ -13,14 +14,16 @@ class MyStation extends StatefulWidget {
 }
 
 class _MyStationState extends State<MyStation> {
+  SheetController controller = SheetController();
   late GoogleMapController mapController;
 
   final Map<String, Marker> _markers = {};
-  bool isExpanded = false;
+  
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           actions: [
             IconButton(onPressed: () {}, 
@@ -37,11 +40,174 @@ class _MyStationState extends State<MyStation> {
                       fontWeight: FontWeight.w600,
                       height: 1.38,
                         ),
-)),
-body: Stack(
-        children: [
-          // Google Map (Replace with actual Google Maps widget)
-          GoogleMap(
+  )),
+ body: buildSheet(),
+ ));
+
+ }
+ 
+
+
+ Widget buildSheet() {
+  return SlidingSheet(
+    openDuration: const Duration(milliseconds: 900),
+    controller: controller,
+    color: Colors.white,
+    shadowColor: Colors.black26,
+    elevation: 12,
+    maxWidth: 500,
+    cornerRadius: 16,
+    cornerRadiusOnFullscreen: 0.0,
+    closeOnBackdropTap: true,
+    closeOnBackButtonPressed: true,
+    addTopViewPaddingOnFullscreen: true,
+    isBackdropInteractable: true,
+    border: Border.all(color: Colors.grey.shade300,
+    width: 3,
+    ),
+    snapSpec: const SnapSpec(
+      snap: true,
+      positioning: SnapPositioning.relativeToAvailableSpace,
+      snappings: [
+        0.1,
+        0.6,
+        1,
+      ],
+    ),
+    // liftOnScrollHeaderElevation: 12.0,
+    body: _buildBody(),
+    headerBuilder: buildheader,
+    builder: buildChild,
+
+  );
+ }
+
+
+Widget buildheader(BuildContext context, SheetState state) {
+  return Column(
+    children: [
+      const SizedBox(height: 2,),
+      Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 16,
+          height: 4,
+           decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2),
+                color: Colors.grey.withOpacity(
+                .5,
+              ),
+              ),
+        ),
+      ),
+      const SizedBox(height: 5,),
+      const  SingleChildScrollView(
+         scrollDirection: Axis.horizontal,
+           child: Row(
+            children: [
+               SizedBox(width: 16.0,),
+               MapIcons(imagePath: 'lib/Icons/drone.png', labelText: 'Drone Spray'),
+               SizedBox(width: 24.0,),
+               MapIcons(imagePath: 'lib/Icons/transport.png', labelText: 'Transport'),
+               SizedBox(width: 24.0,),
+               MapIcons(imagePath: 'lib/Icons/Warehouse.png', labelText: 'Storage'),
+               SizedBox(width: 24.0,),
+               MapIcons(imagePath: 'lib/Icons/Farmer.png', labelText: 'Manpower'),
+               SizedBox(width: 24.0,),
+               MapIcons(imagePath: 'lib/Icons/Report.png', labelText: 'Soil Texting'),
+             
+              ],
+            )
+         ),
+    const SizedBox(height: 5,),
+    Container(
+    height: 1,
+    color: Colors.grey.shade300,
+  )
+    ],
+  );
+}
+
+Widget buildChild(BuildContext context, SheetState state) {
+  final divider = Container(
+    height: 1,
+    color: Colors.grey.shade300,
+  );
+
+  return SingleChildScrollView(
+    scrollDirection: Axis.vertical,
+    child: Column(
+      // mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+      divider,
+       const Padding(
+            padding: EdgeInsets.all(14.0),
+          child: Text('Available Drone Spraying',
+            style: TextStyle(
+           color: Color(0xFF200E32),
+            fontSize: 16,
+            fontFamily: 'Mukta',
+            fontWeight: FontWeight.w500,
+            height: 1.38,
+            ),
+            ),
+           ),
+           const Padding(
+             padding: EdgeInsets.all(8.0),
+             child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$51'),
+           ),
+            const Padding(
+             padding: EdgeInsets.all(8.0),
+             child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$32'),
+           ),
+            const Padding(
+             padding: EdgeInsets.all(8.0),
+             child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$32'),
+           ),   
+           const Padding(
+             padding: EdgeInsets.all(14.0),
+             child: Text(
+            'Recommended for you',
+            style: TextStyle(
+            color: Color(0xFF200E32),
+            fontSize: 16,
+            fontFamily: 'Mukta',
+            fontWeight: FontWeight.w500,
+            height: 1.38,
+            ),
+            ),
+           ),    
+       const SingleChildScrollView(
+         scrollDirection: Axis.horizontal,
+         child: Row(
+           children: [
+             SizedBox(width: 10.0,),
+             RecommendationCard(
+    imagePath: 'lib/Icons/farmland.png',
+    title: 'Book Drone Spray',
+    description: 'Spray your farms with smart precision spraying technology.',
+               ),
+               
+               RecommendationCard(
+    imagePath: 'lib/Icons/Tractor.png',
+    title: 'Book Tractor',
+    description: 'Spray your farms with smart precision spraying technology.',
+    ),
+              
+      
+    ],)
+    )
+    ]
+    ),
+  );
+
+}
+
+Widget _buildBody() {
+  
+  return Stack(
+    children: <Widget>[
+      GoogleMap(
             initialCameraPosition: const CameraPosition(
               target: currentLocation,
               zoom: 16,
@@ -52,153 +218,25 @@ body: Stack(
               },
               markers: _markers.values.toSet(),
             ),
-
-          Positioned.fill(
-              child: Container(
-               padding: const EdgeInsets.only(top: 16.0),
-              color: Colors.transparent, 
-              child: Column(
-                children: [
-                  // Search Bar
-                  SingleChildScrollView(
-                    child: Container(
-                      margin: const EdgeInsets.all(16.0),
-                      color: Colors.white,
-                      child: TextField(
-                        
-                        decoration: InputDecoration(
-                          hintText: 'Search Area, Locality',
-                          suffixIcon: const Icon(Icons.search),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
+      Container(
+                    margin: const EdgeInsets.all(16.0),
+                    color: Colors.white,
+                    child: TextField(
+                      
+                      decoration: InputDecoration(
+                        hintText: 'Search Area, Locality',
+                        suffixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 200.0),
-                  // Content Container
-                  Expanded(
-                    
-                 child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(5.0),
-                            width: double.infinity,
-                            // height: double.infinity,
-                            height: MediaQuery.of(context).size.height,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: const BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 2,
-                                  blurRadius: 5,
-                                  offset: const Offset(0, 3),
-                                ),
-                              ],
-                            ),
-                            child: const Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SingleChildScrollView(
-                                  scrollDirection: Axis.horizontal,
-                                  child: Row(
-                                    
-                                    children: [
-                                      SizedBox(width: 16.0,),
-                                      MapIcons(imagePath: 'lib/Icons/drone.png', labelText: 'Drone Spray'),
-                                      SizedBox(width: 24.0,),
-                                      MapIcons(imagePath: 'lib/Icons/transport.png', labelText: 'Transport'),
-                                      SizedBox(width: 24.0,),
-                                      MapIcons(imagePath: 'lib/Icons/Warehouse.png', labelText: 'Storage'),
-                                      SizedBox(width: 24.0,),
-                                      MapIcons(imagePath: 'lib/Icons/Farmer.png', labelText: 'Manpower'),
-                                      SizedBox(width: 24.0,),
-                                      MapIcons(imagePath: 'lib/Icons/Report.png', labelText: 'Soil Texting'),
-                                
-                                  ],),
-                                ),
-                                SizedBox(height: 20,),
-                                Padding(
-                                  padding: EdgeInsets.all(14.0),
-                                  child: Text(
-                                              'Available Drone Spraying',
-                                              style: TextStyle(
-                                              color: Color(0xFF200E32),
-                                              fontSize: 16,
-                                              fontFamily: 'Mukta',
-                                              fontWeight: FontWeight.w500,
-                                              height: 1.38,
-                                              ),
-                                              ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$51'),
-                                ),
-                                 Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$32'),
-                                ),
-                                 Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: DetailCard(imagePath: 'lib/Icons/drones.jpg', equipment: 'Krishakthi', description: '  drones with smart nozzle spraying starting from ', price: '\$32'),
-                                ),   
-                                Padding(
-                                  padding: EdgeInsets.all(14.0),
-                                  child: Text(
-                                              'Recommended for you',
-                                              style: TextStyle(
-                                              color: Color(0xFF200E32),
-                                              fontSize: 16,
-                                              fontFamily: 'Mukta',
-                                              fontWeight: FontWeight.w500,
-                                              height: 1.38,
-                                              ),
-                                              ),
-                                ),    
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: [
-                                  SizedBox(width: 10.0,),
-                                  RecommendationCard(
-                                      imagePath: 'lib/Icons/farmland.png',
-                                      title: 'Book Drone Spray',
-                                      description: 'Spray your farms with smart precision spraying technology.',
-                                    ),
-                                    
-                                    RecommendationCard(
-                                      imagePath: 'lib/Icons/Tractor.png',
-                                      title: 'Book Tractor',
-                                      description: 'Spray your farms with smart precision spraying technology.',
-                                    ),
-                                   
-                                    
-                                ],
-                              ),
-                            )
-                                  
-                            ]),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    )
-    );
-  }
-  addMarker(String id, LatLng location) {
+                  )
+    ]
+    ,);
+
+}
+addMarker(String id, LatLng location) {
     var marker = Marker(markerId: MarkerId(id),
     position: location,
     infoWindow: const InfoWindow(
